e58c99f2c4849a598e1d4d4162d18d4931a3d37d
Jesus Romero <jesusr1297@outlook.com>
2022-05-26 08:44:41 -0700

    Added blog to url as home page and created an about section

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# module to import urls from other apps
+from django.urls import include
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls'))
 ]
