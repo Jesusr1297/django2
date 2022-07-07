4afa45d010faa9c07ea851bd9287364c7b17c013
Jesus Romero <jesusr1297@outlook.com>
2022-07-07 14:57:32 -0500

    Added email and password recovery features, added button to login page

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -147,3 +147,9 @@ MEDIA_URL = '/media/'
 # Where our media is stored locally
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'jarc1216@gmail.com'  # os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
